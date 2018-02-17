b1e367573d93dde0c58de6475d90f88de97087e3
addonmis <mulyashov.moris@gmail.com>
2018-02-17 13:54:33 +0000

    configured the server

---
+++ server/server.js
@@ -0,0 +1,7 @@
+import app from "./app";
+import { PORT } from "./config";
+
+const server = app.listen(PORT, err => {
+    if (err) console.log(err);
+    console.log(`server running on: ${PORT}`);
+});
